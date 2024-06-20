ead29041d6325c5329fab7765222a93f84554f8f
ofmi <goncarm354@gmail.com>
2024-06-20 14:32:07 +0300

    Range Sum of BST

---
+++ Tree/938_RangeSumOfBST.cpp
@@ -0,0 +1,22 @@
+#include "tree.h"
+
+int rangeSumBST(TreeNode* root, int low, int high) {
+    if (root == nullptr) {
+      return 0;
+    }
+    
+    int sum = 0;
+    if (root->val >= low && root->val <= high) {
+      sum += root->val;
+    }
+
+    if (root->val > low) {
+      sum += rangeSumBST(root->left, low, high);
+    }
+
+    if (root->val < high) {
+      sum += rangeSumBST(root->right, low, high);
+    }
+
+    return sum;
+}
---
+++ Tree/tree.h
@@ -0,0 +1,19 @@
+#define LEETCODE_TREE_H
+#ifdef LEETCODE_TREE_H
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode *root, int low, int high);
+};
+
+#endif  // LEETCODE_TREE_H
