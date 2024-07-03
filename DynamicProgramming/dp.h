7364146e705b131ef1813e96939f8e2fdcf2371e
ofmi <goncarm354@gmail.com>
2024-07-03 22:52:36 +0300

    Minimum Path Sum

--- DynamicProgramming/dp.h
+++ DynamicProgramming/dp.h
@@ -1,5 +1,5 @@
 #ifndef DYNAMIC_PROGRAMMING
-#define DYNAMIC_PROGRAMMING 
+#define DYNAMIC_PROGRAMMING
 
 #include <iostream>
 #include <vector>
@@ -8,6 +8,7 @@ class Solution {
  public:
   int climbStairs(int n);
   std::vector<int> countBits(int n);
+  int minPathSum(std::vector<std::vector<int>>& grid);
 };
 
 #endif  // DYNAMIC_PROGRAMMING
