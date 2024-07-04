61d55d141bc8f523e377317a1cac7c109a421227
ofmi <goncarm354@gmail.com>
2024-07-04 11:52:59 +0300

    Fibonacci Number

--- DynamicProgramming/dp.h
+++ DynamicProgramming/dp.h
@@ -9,6 +9,7 @@ class Solution {
   int climbStairs(int n);
   std::vector<int> countBits(int n);
   int minPathSum(std::vector<std::vector<int>>& grid);
+  int fib(int n);
 };
 
 #endif  // DYNAMIC_PROGRAMMING
