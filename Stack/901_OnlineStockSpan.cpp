aceecbe860c85422542de1e7d57eefc5d240d1fc
ofmi <goncarm354@gmail.com>
2024-08-23 19:06:36 +0300

    Online Stock Span

---
+++ Stack/901_OnlineStockSpan.cpp
@@ -0,0 +1,19 @@
+#include "stack.h"
+
+class StockSpanner {
+ private:
+  std::stack<std::pair<int, int>> s;
+
+ public:
+  StockSpanner() {};
+
+  int next(int price) {
+    int span = 1;
+    while (!s.empty() && s.top().first <= price) {
+      span += s.top().second;
+      s.pop();
+    }
+    s.push({price, span});
+    return span;
+  }
+};
